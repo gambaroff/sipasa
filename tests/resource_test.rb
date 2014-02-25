abd55cd218009b394e79b7549925291eb6d41045
Stephania Gambaroff Thomas <stgambar@gambaroff.com>
2014-02-25 13:49:00 -0800

    Add tests

--- tests/resource_test.rb
+++ tests/resource_test.rb
@@ -8,12 +8,18 @@ class TestResource <  Test::Unit::TestCase
     @ranges = '["192.168.2.0/24", ["192.168.3.2", "192.168.3.9"]]'
     sut = IpResource.new(@ranges)
     puts sut.ip_ranges
+    assert_equal Range, @ranges[0].class #fails cuase it is currently a String
+    assert_equal Range, @ranges[1].class #Fails cause it is caurrently an Array
+    assert_equal ip_ranges[0], ip_ranges["192.168.2.0..192.168.2.255"]
+    assert_equal ip_ranges[1], ip_ranges["192.168.3.2..192.168.3.9"]
     # actually do this test
     # ip_ranges should be an array with 2 entries.  Each entry is a ranges:
     # 192.168.2.0..192.168.2.255
     # and
     # 192.168.3.2..192.168.3.9
   end
+  
+
 end
 
 =begin
