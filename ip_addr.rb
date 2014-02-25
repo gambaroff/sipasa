434c52a7a1618a035ed991b1fefedc7f3cc0399a
Dave Thomas <david.thomas@verifi.com>
2014-02-25 12:16:35 -0800

    added testing and resource class to be tested

--- ip_addr.rb
+++ ip_addr.rb
@@ -4,18 +4,10 @@ require 'ipaddr'
 #ip_range = IPAddr.new("192.168.2.0")..IPAddr.new("192.168.2.6")
 
 world = { ranges: [], used: [] }
-
-ipfile = File.open("ipworld.json", "w")
-ipfile.puts world.to_json
-ipfile.close
-
-ipfile2 = File.open("ipworld.json", "r")
-myworld = JSON.parse(ipfile2.read)
-
 class IPGenerator
   def initialize(world)
     @range = world.each{
-      
+
     }
     @used
   end
@@ -27,15 +19,14 @@ class IpPool
   end
 end
 
-class Ip < ActiveRecord::Base
-  def initialize()
+class IpResource
+  attr_reader :ip_ranges
+  def initialize(json)
+    @ip_ranges = json.collect do |range|
+      range.is_a?(String) ? IPAddr.new(range).to_range : IPAddr.new(range[0])..IPAddr.new(range[1])
+    end
   end
-end
 
-ranges = ["192.168.2.0/24", ["192.168.3.2", "192.168.3.9"]]
- 
-ip_ranges = ranges.collect do |range|
-  range.is_a?(String) ? IPAddr.new(range).to_range : IPAddr.new(range[0])..IPAddr.new(range[1])
 end
 
 
