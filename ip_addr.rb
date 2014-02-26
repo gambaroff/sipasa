b5e62c480d755443a94e0f4c1e792e1b639f8ab7
Stephania Gambaroff Thomas <stgambar@gambaroff.com>
2014-02-26 06:50:34 -0800

    fix test errors

--- ip_addr.rb
+++ ip_addr.rb
@@ -17,7 +17,7 @@ class Pool
   attr_reader :name, :range
   def initialize(name, ip_addr_range)
     @name = name
-    @range = range.is_a?(String) ? IPAddr.new(range).to_range : IPAddr.new(range[0])..IPAddr.new(range[1])
+    @range = ip_addr_range.is_a?(String) ? IPAddr.new(ip_addr_range).to_range : IPAddr.new(ip_addr_range[0])..IPAddr.new(ip_addr_range[1])
   end
 end
 
@@ -54,27 +54,25 @@ class GraphFactory
     @interfaces = {}
     @ips = {}  
     @hosts = {}
-    @pools = resource['pools'].collect do |poolname, pool|
-      pool = Pool.new(poolname, pool['range'])
-      pool['interfaces'].each do |interface, entry|
+    @pools = resource['pools'].collect do |poolname, poolentries|
+      pool = Pool.new(poolname, poolentries['range'])
+      poolentries['interfaces'].each do |interface, entry|
         ip_addr=entry['ip_addr']
-        if @ips.contains?(ip_addr) 
-          ip = @ips[ip_addr]
-        else
+        ip = @ips[ip_addr]
+        if ip == nil
           ip =  IP.new(ip_addr)
           @ips[ip_addr] = ip
         end
         hostname = entry['host']
         if hostname != nil
-          if @hosts.contains?(hostname) 
-            host = @hosts[hostname]
-          else
+          host = @hosts[hostname]
+          if host == nil
             host =  Host.new(hostname)
             @hosts[hostname] = host
           end
         end
         #do something with IP
-        if @interfaces.contains?(interface)
+        if @interfaces[interface] != nil
           raise "duplicate interfaces exist"
         end
         @interfaces[interface] = Interface.new(interface, ip, entry['mac'], entry['type'], host, entry['requested_time'])
--- tests/resource_test.rb
+++ tests/resource_test.rb
@@ -15,14 +15,14 @@ class TestResource <  Test::Unit::TestCase
         "interfaces":{}
        }
      }}'
-    sut = GraphFactory.new.read(@ranges)
-    output = sut.pools
-    assert_equal Range, output[0].class #fails cuase it is currently a String
-    assert_equal Range, output[1].class #Fails cause it is caurrently an Array
-    assert_equal IPAddr.new("192.168.2.0"), output[0].first
-    assert_equal IPAddr.new("192.168.2.255"), output[0].last
-    assert_equal IPAddr.new("192.168.3.2"), output[1].first
-    assert_equal IPAddr.new("192.168.3.9"), output[1].last
+    @pools, @interfaces, @ips, @hosts = GraphFactory.new.read(@ranges)
+    output = @pools
+    assert_equal Range, output[0].range.class #fails cuase it is currently a String
+    assert_equal Range, output[1].range.class #Fails cause it is caurrently an Array
+    assert_equal IPAddr.new("192.168.2.0"), output[0].range.first
+    assert_equal IPAddr.new("192.168.2.255"), output[0].range.last
+    assert_equal IPAddr.new("192.168.3.2"), output[1].range.first
+    assert_equal IPAddr.new("192.168.3.9"), output[1].range.last
   end
   
   def test_entry_creation
@@ -44,8 +44,8 @@ class TestResource <  Test::Unit::TestCase
               "requested": "Tue Feb 25 14:57:35 PST 2014"
             }
           }}}}'
-    sut = GraphFactory.new.read(@input)
-    assert_equal sut.interfaces, "192.168.2.0 : 192.168.2.1" 
+    @pools, @interfaces, @ips, @hosts = GraphFactory.new.read(@input)
+    assert_equal @interfaces, "192.168.2.0 : 192.168.2.1" 
     #puts sut.ip_addresses #should have 192.168.2.1, 192.168.2.2
   end
   
@@ -77,7 +77,7 @@ class TestResource <  Test::Unit::TestCase
               "requested": "Tue Feb 25 14:57:35 PST 2014"
             }
           }}}}'
-    sut = GraphFactory.new.read(@input)
+    @pools, @interfaces, @ips, @hosts = GraphFactory.new.read(@input)
     #puts sut.hosts # should have cheddarcheese with 3 interfaces
   end
 end
